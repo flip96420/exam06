3ea5f0b27f8620dc82672b6ca5553584db9ac4d8
Philipp Schmunk <pschmunk@c2r8s6.42wolfsburg.de>
2026-06-02 22:27:00 +0200

    asdhajksdhjashd

--- test.c
+++ test.c
@@ -9,11 +9,15 @@
 #include <sys/socket.h>
 #include <sys/select.h>
 
-int		count = 0, max_fd = 0;
+int		count = 0, max_fds = 0;
+
 int		ids[65536];
 char	*msgs[65536];
+
 fd_set	r_fds, w_fds, a_fds;
-char	buf_read[1001], buf_write[42];
+
+char	read_buf[1001], write_buf[42];
+
 
 // COPIED FROM MAIN.C START
 int extract_message(char **buf, char **msg)
@@ -64,6 +68,7 @@ char *str_join(char *buf, char *add)
 }
 // COPIED FROM MAIN.C END
 
+
 void	fatal_error()
 {
 	write(2, "Fatal error\n", 12);
@@ -72,53 +77,53 @@ void	fatal_error()
 
 void	notify_others(int author, char *msg)
 {
-	for (int i = 0; i <= max_fd; i++)
+	for (int fd = 0; fd <= max_fds; fd++)
 	{
-		if (FD_ISSET(i, &w_fds) && i != author)
-			send(i, msg, strlen(msg), 0);
+		if(FD_ISSET(fd, &w_fds) && fd != author)
+			send(fd, msg, strlen(msg), 0);
 	}
 }
 
 void	register_client(int fd)
 {
-	max_fd = fd > max_fd ? fd : max_fd;
+	max_fds = fd > max_fds ? fd : max_fds;
 	ids[fd] = count++;
 	msgs[fd] = NULL;
 
 	FD_SET(fd, &a_fds);
-	sprintf(buf_write, "server: client %d just arrived\n", ids[fd]);
-	notify_other(fd, buf_write);
+	sprintf(write_buf, "server: client %d just arrived\n", ids[fd]);
+	notify_others(fd, write_buf);
 }
 
 void	remove_client(int fd)
 {
-	sprintf(buf_write, "server: client %d just left\n", ids[fd]);
-	notify_others(fd, buf_write);
+	sprintf(write_buf, "server: client %d just left\n", ids[fd]);
+	notify_others(fd, write_buf);
 	free(msgs[fd]);
 	FD_CLR(fd, &a_fds);
 	close(fd);
 }
 
-void	send_msg(int fd)
+void	send_message(int fd)
 {
 	char *msg;
 
 	while (extract_message(&(*msgs[fd]), &msg))
 	{
-		sprintf(buf_write, "client %d: ", ids[fd]);
-		notify_others(fd, buf_write);
+		sprintf(write_buf, "client %d: ", ids[fd]);
+		notify_others(fd, write_buf);
 		notify_others(fd, msg);
 		free(msg);
 	}
 }
 
-int create_socket()
+int		create_socket()
 {
-	max_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (max_fd < 0)
+	max_fds = socket(AF_INET, SOCK_STREAM, 0);
+	if (max_fds < 0)
 		fatal_error();
-	FD_SET(max_fd, &a_fds);
-	return (max_fd);
+	FD_SET(max_fds, &a_fds);
+	return (max_fds);
 }
 
 int main(int ac, char **av)
@@ -129,43 +134,57 @@ int main(int ac, char **av)
 		exit(1);
 	}
 
-	// COPIED FROM MAIN.C START
 	FD_ZERO(&a_fds);
-	int sockfd = create_socket();
-
 	struct sockaddr_in servaddr;
+
+	int sockfd = create_socket();
 	bzero(&servaddr, sizeof(servaddr));
 
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(2130706433);
-	servaddr.sin_port = htons(atoi(av[1])); // replace 8080
+	// assign IP, PORT 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
+	servaddr.sin_port = htons(atoi(av[1])); 
 
-	if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)))
+	// Binding newly created socket to given IP and verification 
+	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
 		fatal_error();
-	if (listen(sockfd, SOMAXCONN)) // the main uses 10, SOMAXCONN is 180 on my machine
+	if (listen(sockfd, SOMAXCONN) != 0)
 		fatal_error();
-	// COPIED FROM MAIN.C END
-
 
 	while (1)
 	{
 		r_fds = w_fds = a_fds;
 
-		if (select(max_fd + 1, &r_fds, &w_fds, NULL, NULL) < 0)
+		if (select(max_fds + 1, &r_fds, &w_fds, NULL, NULL) < 0)
 			fatal_error();
 		
-		for (int fd = 0; fd <= max_fd; fd++)
+		for (int fd = 0; fd <= max_fds; fd++)
 		{
 			if (!FD_ISSET(fd, &r_fds))
 				continue;
 			
-				if (fd == sockfd)
+			if (fd == sockfd)
+			{
+				int client_fd = accept(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+				if (client_fd >= 0)
 				{
-					socklen_t addr_len = sizeof(servaddr);
-					int client_fd = 
+					register_client(fd);
+					break;
 				}
+			}
+			else
+			{
+				int read_bytes = recv(fd, read_buf, 1000, 0);
+				if (read_bytes <= 0)
+				{
+					remove_client(fd);
+					break;
+				}
+				read_buf[read_bytes] = '\0';
+				msgs[fd] = str_join(msgs[fd], read_buf);
+				send_message(fd);
+			}
 		}
 	}
-
 	return (0);
 }
